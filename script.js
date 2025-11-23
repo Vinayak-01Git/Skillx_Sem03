55a11a7f765fda0f26f97563853d73153211b42f
Vinayak-01Git <vinayakkeshri207@gmail.com>
2025-11-23 22:01:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// to change the color of the button (onclick) using js.
+// function Find(){
+//     document.getElementById("demo").style.color="red";
+// }
+
+function Redirect(){
+  window.location.href="index01.html";
+    
+}
+function Redirect2(){
+  window.location.href="index02.html";
+    
+}
